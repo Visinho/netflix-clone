9355d3efde24bd644ed267c042550b349e209aac
Visinho <Elvisvisinho@rocketmail.com>
2023-07-22 15:10:18 +0100

    Completed the design of the Signin page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
 import Input from '@/components/Input';
 
-type Props = {};
 
-const Page = (props: Props) => {
+const Page = () => {
+
+  const [email, setEmail ] = useState("");
+  const [name, setName ] = useState("");
+  const [password, setPassword ] = useState("");
+
   return (
     <div className='relative h-full w-full bg-[url("/images/hero.jpg")] bg-center bg-fixed bg-cover'>
       <div className='bg-black w-full h-full lg:bg-opacity-50'>
@@ -15,8 +21,21 @@ const Page = (props: Props) => {
           <div className='bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full'>
             <h2 className='text-white text-4xl mb-8 font-semibold'>Sign In</h2>
             <div className='flex flex-col gap-4'>
-              <Input />
+              <Input label='Username' onChange={(e: any) => setName(e.target.value)} id='name' value={name}/>
+              <Input label='Email' onChange={(e: any) => setEmail(e.target.value)} id='email' type='email' value={email}/>
+              <Input label='Password' onChange={(e: any) => setPassword(e.target.value)} id='password' type='password' value={password}/>
             </div>
+
+            <button className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-900 transition'>
+                Login
+            </button>
+            <p className='text-neutral-500 mt-12'>First time using Netflix?
+              <span className='text-white ml-0.5 hover:underline cursor-pointer'>
+              Create an account
+            </span>
+            </p>
+            
+
           </div>
         </div>
 
