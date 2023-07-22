3921295552324cff073d936e34d42062e4742f29
Visinho <Elvisvisinho@rocketmail.com>
2023-07-22 14:43:14 +0100

    Designed the Signin Input component

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Input from '@/components/Input';
 
 type Props = {};
 
@@ -13,6 +14,9 @@ const Page = (props: Props) => {
         <div className='flex justify-center'>
           <div className='bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full'>
             <h2 className='text-white text-4xl mb-8 font-semibold'>Sign In</h2>
+            <div className='flex flex-col gap-4'>
+              <Input />
+            </div>
           </div>
         </div>
 
